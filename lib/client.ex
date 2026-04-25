c99c176e2cbceeed593d28e4ab39a85e7fe923c3
yutingrui <yutingrui@tal>
2026-04-25 18:40:26 +0800

    fix(elixir): restore Service and Stub definitions in client.ex

--- lib/client.ex
+++ lib/client.ex
@@ -1,3 +1,14 @@
+defmodule Demo.V1.DemoService.Service do
+  use GRPC.Service, name: "demo.v1.DemoService", protoc_gen_elixir_version: "0.15.0"
+
+  rpc(:Echo, Demo.V1.EchoRequest, Demo.V1.EchoResponse)
+  rpc(:Health, Google.Protobuf.Empty, Demo.V1.HealthResponse)
+end
+
+defmodule Demo.V1.DemoService.Stub do
+  use GRPC.Stub, service: Demo.V1.DemoService.Service
+end
+
 defmodule DemoClient do
   alias Demo.V1.{EchoRequest, EchoResponse, HealthResponse}
   alias Demo.V1.DemoService.Stub
