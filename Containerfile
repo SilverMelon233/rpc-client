df86b81655a5f96085ec1ffd063f905d87644703
yutingrui <yutingrui@tal>
2026-04-25 09:15:52 +0800

    init(golang): server at root

---
+++ Containerfile
@@ -0,0 +1,11 @@
+FROM dhi.io/golang:1-alpine3.23-dev AS build
+WORKDIR /src
+COPY go.mod ./
+COPY client.go ./
+RUN GOFLAGS=-mod=mod GOPROXY=direct go get github.com/SilverMelon233/rpc-stub/golang@main && \
+    GOFLAGS=-mod=mod GOPROXY=direct go build -o /client .
+
+FROM dhi.io/alpine-base:3.23
+COPY --from=build /client /client
+ENV SERVER_ADDR=server:50051
+ENTRYPOINT ["/client"]
---
+++ client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	demov1 "github.com/SilverMelon233/rpc-stub/golang/gen/demo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func main() {
+	addr := os.Getenv("SERVER_ADDR")
+	if addr == "" {
+		addr = "server:50051"
+	}
+
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := demov1.NewDemoServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	echoResp, err := client.Echo(ctx, &demov1.EchoRequest{Message: "Hello from Go!"})
+	if err != nil {
+		log.Fatalf("Echo failed: %v", err)
+	}
+	log.Printf("Echo response: %s", echoResp.GetMessage())
+
+	healthResp, err := client.Health(ctx, &emptypb.Empty{})
+	if err != nil {
+		log.Fatalf("Health failed: %v", err)
+	}
+	log.Printf("Health response: %s", healthResp.GetStatus())
+}
