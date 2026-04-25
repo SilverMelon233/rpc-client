d7e37dfb51fb2408caefa2209d7cc3751452d291
yutingrui <yutingrui@tal>
2026-04-25 18:28:16 +0800

    fix(elixir): add GRPC.Client.Supervisor to supervision tree

--- lib/application.ex
+++ lib/application.ex
@@ -3,8 +3,15 @@ defmodule DemoClient.Application do
 
   @impl true
   def start(_type, _args) do
+    children = [
+      {GRPC.Client.Supervisor, []}
+    ]
+    opts = [strategy: :one_for_one, name: DemoClient.Supervisor]
+    {:ok, pid} = Supervisor.start_link(children, opts)
+
     DemoClient.run()
     System.halt(0)
-    Supervisor.start_link([], strategy: :one_for_one)
+
+    {:ok, pid}
   end
 end
